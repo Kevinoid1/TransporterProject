a049c7e3061df8a98af3949fb29e917832889da5
Kevin Eboka <nonso.eboka@gmail.com>
2021-07-23 10:28:24 +0100

    Initial Push

---
+++ Transporter/Transporter/Controllers/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Transporter.Controllers
+{
+    public class LoginViewModel
+    {
+        public string Password { get; internal set; }
+        public string Email { get; internal set; }
+    }
+}
